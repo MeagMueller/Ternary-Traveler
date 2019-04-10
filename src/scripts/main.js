60cd3ac96e3b1549d6ed83a089884657e7bee66a
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-04-10 11:39:31 -0500

    Made notes

---
+++ src/scripts/main.js
@@ -0,0 +1,6 @@
+// 1. user already has points of interest, when application is opened, all points of interest should be displayed with name, description, cost, review if it isn't blank, and the place it is located
+// 2. user wants to add new point of interest, create form for point of interest, description, cost, dropdown
+// 3. user wants to change point of interest, perform gesture to edit, present user with prefilled form if not previously blank and affordance to save
+// 4. give user ability to delete from interest display
+// 5. give user confirmation ("are you sure") to delete or not - cancel should not delete 
+
