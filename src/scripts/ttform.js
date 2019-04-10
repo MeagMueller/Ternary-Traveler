67527bebf3bc096d74454af4281cf12ca3c069a9
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-04-10 13:36:47 -0500

    Made files, began building form and fetch calls

---
+++ src/scripts/ttform.js
@@ -0,0 +1,27 @@
+const ttform = {
+    addNewTravelPointForm() {
+        const newTravelPointDisplay = document.createElement("section")
+        display-container.appendChild(newTravelPointDisplay)
+
+        const newTravelNameLabel = document.createElement("label")
+        newTravelNameLabel.textContent = ("Name")
+        newTravelPointDisplay.appendChild(newTravelNameLabel)
+
+        const newTravelNameInput = document.createElement("input")
+        newTravelNameInput.setAttribute = ("id", "newTravelNameInput")
+        newTravelPointDisplay.appendChild(newTravelNameInput)
+
+        const newTravenDestinationLabel = document.createElement("label")
+        newTravelPointDisplay.appendChild(newTravelDestinationLabel)
+
+        const newTravelDescriptionInput = document.createElement("input")
+        newTravelDestinationInput.setAttribute = ("id", "newTravelDestinationInput")
+        newTravelPointDisplay.appendChild(newTravelDescriptionInput)
+
+        
+
+        
+    }
+}
+
+export default ttform
